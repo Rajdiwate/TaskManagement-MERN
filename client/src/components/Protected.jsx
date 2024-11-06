8428f460e296d89fbfb73eb6a86c9feed760649e
Rajdiwate <127538240+Rajdiwate@users.noreply.github.com>
2024-11-06 17:32:36 +0530

    bug fix

--- client/src/components/Protected.jsx
+++ client/src/components/Protected.jsx
@@ -4,11 +4,11 @@ import { Navigate } from 'react-router-dom';
 
 const Protected = ({ children }) => {
   const { isAuthenticated, loading } = useSelector((state) => state.user);
+   
 
   if (loading) {
     return <div>Loading...</div>;
   }
-
   if (!isAuthenticated) {
     return <Navigate to="/auth/login" replace />;
   }
